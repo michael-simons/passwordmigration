adcd9f96bde9669ffbf750f6473d3e2bf71fe984
Michael J. Simons <michael@simons.ac>
2018-01-13 00:50:48 +0100

    Add documentation

--- src/main/java/ac/simons/spring/passwordmigration/PasswordmigrationApplication.java
+++ src/main/java/ac/simons/spring/passwordmigration/PasswordmigrationApplication.java
@@ -15,6 +15,8 @@
  */
 package ac.simons.spring.passwordmigration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEventPublisher;
@@ -26,6 +28,7 @@ import org.springframework.security.authentication.DefaultAuthenticationEventPub
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -56,6 +59,10 @@ public class PasswordmigrationApplication {
 
 }
 
+/**
+ * This is a really bad idea, don't ever use this password encoder at home. It's just here
+ * for illustrating purposes.
+ */
 class BSPasswordEncoder implements PasswordEncoder {
 
     @Override
@@ -76,14 +83,30 @@ class BSPasswordEncoder implements PasswordEncoder {
 @Configuration
 class SecurityConfiguration {
 
+    private static final Logger LOG = LoggerFactory
+        .getLogger(SecurityConfiguration.class);
+
+
+    /**
+     * This configures the new delegating password encoder system. More hints inside the
+     * method.
+     *
+     * @return The systemwide password encoder.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
+        // This is the id of the default encoder that is used for *encoding*
+        // new passwords
         final String idForEncode = "pbkdf2";
-
+        // And that is the actual encoder for the above id. I need this on a variable
+        // later on.
         final Pbkdf2PasswordEncoder defaultEncoder = new Pbkdf2PasswordEncoder();
 
+        // Create the chain of password encoders
         final Map<String, PasswordEncoder> encoders
             = new HashMap<>();
+        // We're gonna use pbkdf2 and some other secure hashes
+        // This reads as: "We can decode all password hashes that starts with one of the keys in the map."
         encoders.put(
             idForEncode, defaultEncoder);
         encoders.put(
@@ -91,28 +114,65 @@ class SecurityConfiguration {
         encoders.put(
             "scrypt", new SCryptPasswordEncoder());
 
-        final DelegatingPasswordEncoder rv = new DelegatingPasswordEncoder(idForEncode, encoders);
+        // The final, delegating password encoder. First parameter configures the default
+        // encoder for matching.
+        final DelegatingPasswordEncoder rv =
+            new DelegatingPasswordEncoder(idForEncode, encoders);
+        // !!!!
+        // That is the crucial part to support "legacy" passworts without a hash
+        // Usually (and hopefully) that would be something like a sha1, maybe md5, but
+        // not the one I configured here ;)
         rv.setDefaultPasswordEncoderForMatches(new BSPasswordEncoder());
         return rv;
     }
 
-    @Bean
-    public AuthenticationEventPublisher authenticationEventPublisher(ApplicationEventPublisher publisher) {
-        return new DefaultAuthenticationEventPublisher(publisher);
-    }
-
+    /**
+     * This is where you connect your user database with the application. For the
+     * purpose of this demo, I'm using an in-memory based solution.
+     * <br>
+     * Notice that I'm not using {@link InMemoryUserDetailsManagerConfigurer} from within
+     * {@link #webSecurityConfigurer(AuthenticationEventPublisher, UserDetailsService)}.
+     * That configurer would have gave me the ability to set the password encoder.
+     * Which would defeat the purpose of this demo, so I use the super secret, incredible
+     * hard to guess password hash <pre>fvzbaf</pre>.
+     *
+     * @return Your custom user details service
+     */
     @Bean
     public UserDetailsService userDetailsService() {
         final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager
             .createUser(withUsername("michael")
-                .password("fvzbaf")
+                .password("fvzbaf") // This is treated as a password hash
                 .roles("user")
                 .build()
             );
         return manager;
     }
 
+    /**
+     * This bean is needed to publish security events like successfull and failed logins.
+     * Spring Boot 2M7 still creates that for you, but that might change in the future.
+     *
+     * @param publisher
+     * @return
+     */
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher(
+        final ApplicationEventPublisher publisher
+    ) {
+        return new DefaultAuthenticationEventPublisher(publisher);
+    }
+
+    /**
+     * This {@link WebSecurityConfigurerAdapter} takes care of all things security.
+     * Take note that as of Spring Boot 2 having one of those in the context, all Spring Boots
+     * magic goes away. All defaults are courtesy of Spring Security itself.
+     *
+     * @param authenticationEventPublisher
+     * @param userDetailsService
+     * @return
+     */
     @Bean
     public WebSecurityConfigurerAdapter webSecurityConfigurer(
         final AuthenticationEventPublisher authenticationEventPublisher,
@@ -121,15 +181,32 @@ class SecurityConfiguration {
         return new WebSecurityConfigurerAdapter() {
             @Override
             protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+                // @formatter:off
                 auth
+                        // That is actually insecure!! But if you want to migrate the password,
+                        // we have to keep the credentials inside the authentication object.
+                        // Luckily for use, the credentials are the clear text password
+                        // in case of username and password authenticatio
                         .eraseCredentials(false)
+                        // Register the authenticationEventPublisher with security, otherwise no events will be fired
                         .authenticationEventPublisher(authenticationEventPublisher)
+                        // Register the rest of our infrastructure.
                         .userDetailsService(userDetailsService)
                         .passwordEncoder(passwordEncoder());
+                // @formatter:on
             }
         };
     }
 
+
+    /**
+     * This method represents the "guts" of this demo: It listens on
+     * {@link AuthenticationSuccessEvent authentication success events} and checks if the authentication inside them
+     * is of a {@link UsernamePasswordAuthenticationToken} with credentials being availabe.
+     *
+     * @param passwordEncoder That is the new, delegating password encoder.
+     * @return
+     */
     @Bean
     public ApplicationListener<AuthenticationSuccessEvent> onSuccessListener(
         final PasswordEncoder passwordEncoder)
@@ -137,12 +214,21 @@ class SecurityConfiguration {
         return (AuthenticationSuccessEvent event) -> {
             final Authentication authentication = event.getAuthentication();
             if (authentication instanceof UsernamePasswordAuthenticationToken && authentication.getCredentials() != null) {
-                System.out.println(authentication.getCredentials() + " -> " + passwordEncoder.encode((CharSequence) authentication.getCredentials()));
+                // That is the point where we are able to retrieve the users login name
+                // and also the clear text password if we didn't let Spring Security erase the credentials
+                // We now can use the delegating password encoder to rehash the password to a safe hash
+                final CharSequence plainTextPassword = (CharSequence) authentication.getCredentials();
+                final String rehashedPassword = passwordEncoder.encode(plainTextPassword);
+
+                LOG.info("Now is the time to store new password hash {} for user {}", rehashedPassword, authentication.getName());
             }
         };
     }
 }
 
+/**
+ * Just a simple controller to try the stuff above.
+ */
 @Controller
 class DemoController {
 
