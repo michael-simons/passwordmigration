0d8ff58a0f1777417e2407366f3884b2051d1335
Michael J. Simons <michael@simons.ac>
2019-08-15 22:37:52 +0200

    Upgrade to Spring Boot 2.1.7 and rely on PasswordEncoder#upgradeEncoding.

--- src/main/java/ac/simons/spring/passwordmigration/PasswordmigrationApplication.java
+++ src/main/java/ac/simons/spring/passwordmigration/PasswordmigrationApplication.java
@@ -15,23 +15,18 @@
  */
 package ac.simons.spring.passwordmigration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.util.stream.Collectors.joining;
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,12 +35,6 @@ import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.util.stream.Collectors.joining;
-import static org.springframework.security.core.userdetails.User.withUsername;
 
 /**
  * @author Michael J. Simons
@@ -82,9 +71,6 @@ class BSPasswordEncoder implements PasswordEncoder {
 @Configuration
 class SecurityConfiguration {
 
-    private static final Logger LOG = LoggerFactory
-        .getLogger(SecurityConfiguration.class);
-
 
     /**
      * This configures the new delegating password encoder system. More hints inside the
@@ -117,7 +103,7 @@ class SecurityConfiguration {
         final DelegatingPasswordEncoder rv =
             new DelegatingPasswordEncoder(idForEncode, encoders);
         // !!!!
-        // That is the crucial part to support "legacy" passworts without a hash
+        // That is the crucial part to support "legacy" passwords without a hash
         // Usually (and hopefully) that would be something like a sha1, maybe md5, but
         // not the one I configured here ;)
         rv.setDefaultPasswordEncoderForMatches(new BSPasswordEncoder());
@@ -129,7 +115,7 @@ class SecurityConfiguration {
      * purpose of this demo, I'm using an in-memory based solution.
      * <br>
      * Notice that I'm not using {@link InMemoryUserDetailsManagerConfigurer} from within
-     * {@link #webSecurityConfigurer(AuthenticationEventPublisher, UserDetailsService)}.
+     * a {@link AuthenticationManagerBuilder#inMemoryAuthentication}.
      * That configurer would have gave me the ability to set the password encoder.
      * Which would defeat the purpose of this demo, so I use the super secret, incredible
      * hard to guess password hash <pre>fvzbaf</pre>.
@@ -137,7 +123,7 @@ class SecurityConfiguration {
      * @return Your custom user details service
      */
     @Bean
-    public UserDetailsService userDetailsService() {
+    public InMemoryUserDetailsManager userDetailsService() {
         final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager
             .createUser(withUsername("michael")
@@ -146,84 +132,6 @@ class SecurityConfiguration {
                 .build()
             );
         return manager;
-    }
-
-    /**
-     * This bean is needed to publish security events like successfull and failed logins.
-     * Spring Boot 2M7 still creates that for you, but that might change in the future.
-     *
-     * @param publisher
-     * @return
-     */
-    @Bean
-    public AuthenticationEventPublisher authenticationEventPublisher(
-        final ApplicationEventPublisher publisher
-    ) {
-        return new DefaultAuthenticationEventPublisher(publisher);
-    }
-
-    /**
-     * This {@link WebSecurityConfigurerAdapter} takes care of all things security.
-     * Take note that as of Spring Boot 2 having one of those in the context, all Spring Boots
-     * magic goes away. All defaults are courtesy of Spring Security itself.
-     *
-     * @param authenticationEventPublisher
-     * @param userDetailsService
-     * @return
-     */
-    @Bean
-    public WebSecurityConfigurerAdapter webSecurityConfigurer(
-        final AuthenticationEventPublisher authenticationEventPublisher,
-        final UserDetailsService userDetailsService
-    ) {
-        return new WebSecurityConfigurerAdapter() {
-            @Override
-            protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-                // @formatter:off
-                auth
-                        // That is actually insecure!! But if you want to migrate the password,
-                        // we have to keep the credentials inside the authentication object.
-                        // Luckily for use, the credentials are the clear text password
-                        // in case of username and password authenticatio
-                        .eraseCredentials(false)
-                        // Register the authenticationEventPublisher with security, otherwise no events will be fired
-                        .authenticationEventPublisher(authenticationEventPublisher)
-                        // Register the rest of our infrastructure.
-                        .userDetailsService(userDetailsService)
-                        .passwordEncoder(passwordEncoder());
-                // @formatter:on
-            }
-        };
-    }
-
-
-    /**
-     * This method represents the "guts" of this demo: It listens on
-     * {@link AuthenticationSuccessEvent authentication success events} and checks if the authentication inside them
-     * is of a {@link UsernamePasswordAuthenticationToken} with credentials being availabe.
-     *
-     * @param passwordEncoder That is the new, delegating password encoder.
-     * @return
-     */
-    @Bean
-    public ApplicationListener<AuthenticationSuccessEvent> onSuccessListener(
-        final PasswordEncoder passwordEncoder)
-    {
-        return (AuthenticationSuccessEvent event) -> {
-            final Authentication authentication = event.getAuthentication();
-            if (authentication instanceof UsernamePasswordAuthenticationToken && authentication.getCredentials() != null) {
-                // That is the point where we are able to retrieve the users login name
-                // and also the clear text password if we didn't let Spring Security erase the credentials
-                // We now can use the delegating password encoder to rehash the password to a safe hash
-                final CharSequence plainTextPassword = (CharSequence) authentication.getCredentials();
-                final String rehashedPassword = passwordEncoder.encode(plainTextPassword);
-
-                LOG.info("Now is the time to store new password hash {} for user {}", rehashedPassword, authentication.getName());
-
-                // Now clear the credentials
-                ((UsernamePasswordAuthenticationToken)authentication).eraseCredentials();
-            }
-        };
     }
 }
 
